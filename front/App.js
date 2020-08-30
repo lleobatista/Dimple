2bec8c7d0963efeaeca25e59ce278813131a99f8
Leonardo Batista <silval.2016@alunos.utfpr.edu.br>
2020-08-30 19:07:38 -0300

    calendario completo com modal

--- front/App.js
+++ front/App.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Login from './src/screens/Login';
-import Calendar from './src/screens/Calendar';
+import CalendarScreen from './src/screens/CalendarScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,7 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator headerMode='none'  >
         <Stack.Screen name='Login' component={Login}  />
-        <Stack.Screen name='Calendar' component={Calendar} options={{ gestureEnabled: false }}/>
+        <Stack.Screen name='Calendar' component={CalendarScreen} options={{ gestureEnabled: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- front/src/screens/CalendarScreen.js
+++ front/src/screens/CalendarScreen.js
@@ -1,64 +1,68 @@
-import React, { Component } from 'react';
-import { View, FlatList, StyleSheet, Text, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList, StyleSheet, Text, TouchableOpacity  } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
+import { Icon } from 'react-native-elements';
 
 import HeaderCalendar from '../components/HeaderCalendar';
 import SafeBottom from '../components/SafeBottom';
-import {Calendar, LocaleConfig} from 'react-native-calendars';
-import { Icon } from 'react-native-elements'
+import Modal from '../components/Modal';
 
-export default class Calendari extends Component {  
 
-  render(){
+const CalendarScreen = () => {  
 
-    LocaleConfig.locales['pt'] = {
-      monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-      monthNamesShort: ['Jan.', 'Fev.', 'Mar.', 'Abr.', 'Mai.', 'Jun.', 'Jul.', 'Ago.', 'Set..', 'Out.', 'Nov.', 'Dez.'],
-      dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
-      dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
-      today: 'Hoje\'hj'
-    };
-
-    LocaleConfig.defaultLocale = 'pt';
-
-    const DATA = [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'Quase',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d7p',
-        title: 'Third Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d02',
-        title: 'Third Item',
-      },
-    ];
+  const [modal, setModal] = useState(false)
   
-    const Item = ({ title }) => (
-      <View style={styles.item}>
-        <Icon
-        name="warning"
-        size={40}
-        color="#FF4A4A"
-        />
-        <Text style={styles.title}>  Todas <Text style={{fontWeight:'300'}}>reservadas</Text> 12:00 até 13:00</Text>
-      </View>
-    );
-  
-    const renderItem = ({ item }) => (
-      <Item title={item.title} />
-    );
+  LocaleConfig.locales['pt'] = {
+    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan.', 'Fev.', 'Mar.', 'Abr.', 'Mai.', 'Jun.', 'Jul.', 'Ago.', 'Set..', 'Out.', 'Nov.', 'Dez.'],
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+    today: 'Hoje\'hj'
+  };
 
+  LocaleConfig.defaultLocale = 'pt';
+  
+    
+
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'Quase',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d7p',
+      title: 'Third Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d02',
+      title: 'Third Item',
+    },
+  ];
+
+  const Item = ({ title }) => (
+    <TouchableOpacity  style={styles.item} onPress={() => setModal(true)} >
+      <Icon
+      name="warning"
+      size={40}
+      color="#FF4A4A"
+      />
+      <Text style={styles.title}>  Todas <Text style={{fontWeight:'300'}}>reservadas</Text> 12:00 até 13:00</Text>
+    </TouchableOpacity >
+  );
+
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+    
     return(
       <View style={{flex:1, flexDirection:'column'}}>
       
@@ -112,28 +116,27 @@ export default class Calendari extends Component {
           }
         }}
         />
-      
         <FlatList
-        style={{marginTop: 10, flex: 1}}
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-        />      
-       
-       <SafeBottom/>
-      <StatusBar style="light" animated={false}/>
-      
-      </View>
-    );
-  }
-}
+          style={{marginTop: 10, flex: 1}}
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
 
+        <Modal 
+          show={modal}
+          close={() => setModal(false)}
+        />
+
+        <SafeBottom/>
+      <StatusBar style="light" animated={false}/>
+      </View>
+    )
+  }
+
+  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   item: {
-    
     flexDirection:'row',
     alignItems:'center',
     marginTop: 2,
@@ -155,5 +158,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 15,
     fontWeight: 'bold'
-  },
-});
+  }
+})
+
+
+export default CalendarScreen;
